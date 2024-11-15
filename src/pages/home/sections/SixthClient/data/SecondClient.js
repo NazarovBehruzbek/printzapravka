ebdb13603d7bb2d4f79cfede5d4fe5f39eef29ba
NazarovBehruzbek <behruzbeknazarov001@gmail.com>
2024-11-15 16:59:14 +0500

    add

--- src/pages/home/sections/SixthClient/data/SecondClient.js
+++ src/pages/home/sections/SixthClient/data/SecondClient.js
@@ -1,6 +1,6 @@
 import image1 from '/src/assets/images/remont-termoprint.png';
 import image2 from '/src/assets/images/remont-plotter.png';
-import image3 from '/src/assets/images/remont-mfu.png';
+import image3 from '/src/assets/images/remont-pk.png';
 
 const SecondClient = [
     {
@@ -16,10 +16,11 @@ const SecondClient = [
         description: 'Ремонт плоттеров любой сложности.  Замена чернильных голов, ремонт механизмов подачи бумаги,  восстановление электронных плат.'
     },
     {
-        title: 'РЕМОНТ МФУ принтеров',
-        text: 'Все бренды',
+        title: 'РЕМОНТ КОМПЬЮТЕРОВ',
+        text: 'Установка Windows программы.',
         images: image3,
-        description: 'Ремонт МФУ принтеров всех брендов.  Замена картриджей,  ремонт печатающей головки,  восстановление  фотобарабана,  настройка  сетей.'
-    }
+        description: 'Опытные специалисты устранят любые неполадки вашего компьютера, от программных сбоев до аппаратных поломок. Гарантия на выполненные работы.'
+    },
+   
 ];
 export default SecondClient;
